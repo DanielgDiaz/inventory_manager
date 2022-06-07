b455ce15e96752af01e1fec0201d37dbbd590138
Danielgdiaz <danielgdiaz92@gmail.com>
2022-06-07 11:17:29 +0200

    table users & migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_101455) do
+ActiveRecord::Schema.define(version: 2022_06_07_090715) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.decimal "price"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.integer "role", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
