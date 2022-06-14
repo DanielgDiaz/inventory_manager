fa973efa2f4dc92dec1c0beb2ed59ec8e1776fb7
Danielgdiaz <danielgdiaz92@gmail.com>
2022-06-14 09:45:03 +0200

    roles in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :password,)
+    params.require(:user).permit(:name, :password, :role)
   end
 
 end
